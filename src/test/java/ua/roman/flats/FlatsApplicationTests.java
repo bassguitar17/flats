ef138edcbf837f01bf59f6fd7bc629775db0a55e
Rumun <bassguitar17@gmail.com>
2021-01-19 10:16:57 +0100

    add initial files

---
+++ src/test/java/ua/roman/flats/FlatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.roman.flats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
